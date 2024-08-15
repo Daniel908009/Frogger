693646d170d81573645dce54c79d110c9e4d84da
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-08-15 17:45:08 +0200

    better background, end screen, obstacles, settings window
    and much more...

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ import tkinter as tk
 def reset():
     pass
 
-# end screen function
+# end screen function, currenlty very simple, will be updated later
 def end_screen():
-    pass
+    end_screen = True
+    while end_screen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                global running
+                running = False
+                end_screen = False
+        screen.fill((255, 255, 255))
+        text = font.render("Score: "+str(score), True, (0, 0, 0))
+        screen.blit(text, (screen.get_width()/2 - text.get_width()/2, screen.get_height()/2 - text.get_height()/2))
+        text1 = font.render("You lost", True, (0, 0, 0))
+        screen.blit(text1, (screen.get_width()/2 - text1.get_width()/2, screen.get_height()/2 - text1.get_height()/2 - text.get_height()))
+        pygame.display.update()
 
 # start screen function
 def start_screen():
@@ -20,6 +32,25 @@ def settings_screen():
     window = tk.Tk()
     window.title("Settings")
     window.geometry("300x300")
+    window.resizable(False, False)
+    window.iconbitmap("settings.ico")
+    # creating a label
+    label = tk.Label(window, text="Settings", font=("Arial", 20))
+    label.pack()
+    # creating a frame for the settings
+    frame = tk.Frame(window)
+    frame.pack()
+    # creating a label for the first setting
+    label1 = tk.Label(frame, text="Setting 1")
+    label1.grid(row=0, column=0)
+    # creating a checkbutton for the first setting
+    checkbutton1 = tk.Checkbutton(frame)
+    checkbutton1.grid(row=0, column=1)
+
+    # creating a apply button
+    apply_button = tk.Button(window, text="Apply", command=lambda: window.destroy())# later this will call a function to apply the settings
+    apply_button.pack(side="bottom")
+
     window.mainloop()
 
 # fill background function(adding obstacles or logs to the background)
@@ -28,7 +59,6 @@ def fill_background(background):
     coordinates = []
     for i in range(background.rect.y, background.rect.y+ background.image.get_height(), 50):
         coordinates.append(i)
-        print(coordinates)
     # for now only adding logs to everything
     temp= 0
     for i in coordinates:
@@ -37,8 +67,14 @@ def fill_background(background):
             direction = "left"
         else:
             direction = "right"
-        log = Log(i, direction)
-        log_group.add(log)
+        if background.type == "water":
+            log = Log(i, direction)
+            log_group.add(log)
+        elif background.type == "road":
+            obstacle = Obstacle(i, direction)
+            obstacle_group.add(obstacle)
+        elif background.type == "grass":
+            pass
 
 # Classes
 # Player class
@@ -78,20 +114,27 @@ class Player(pygame.sprite.Sprite):
         for background in backgrounds_group:
             if pygame.sprite.collide_rect(self, background):
                 self.on_what = background.type
-        #print(self.on_what)
 
 # obstacle class
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, y, direction):
         super().__init__()
-        self.image = pygame.image.load('obstacle.png')
+        self.image = resized_obstacle
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, 800)
-        self.rect.y = random.randint(0, 600)
-        self.speed = random.randint(1, 5)
-
+        self.rect.y = y
+        self.speed = random.randint(1, 3)
+        self.direction = direction
+        if self.direction == "left":
+            self.rect.x = screen.get_width()+random.randint(0, obstacle_image.get_width()*2)
+            self.image = pygame.transform.flip(self.image, True, False)
+        elif self.direction == "right":
+            self.rect.x = 0 - self.rect.width - random.randint(0, obstacle_image.get_width()*2)
+    
     def move(self):
-        self.rect.x -= self.speed
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        elif self.direction == "right":
+            self.rect.x += self.speed
         self.rect.y = self.rect.y + change_in_y
     
     def draw(self, screen):
@@ -104,7 +147,7 @@ class Log(pygame.sprite.Sprite):
         self.image = resized_log
         self.rect = self.image.get_rect()
         self.rect.y = y
-        self.speed = 1 # also will later be randomized
+        self.speed = random.randint(1, 3)
         self.direction = direction
         if self.direction == "left":
             self.rect.x = screen.get_width()+random.randint(0, log_image.get_width()*2)
@@ -132,18 +175,21 @@ class Log(pygame.sprite.Sprite):
 
 # background class
 class Background(pygame.sprite.Sprite):
-    def __init__(self, type, y):
+    def __init__(self, type, y, height):
         super().__init__()
         if type == None:
             self.type = random.choice(["road", "water", "grass"])
         else:
             self.type = type
-        if self.type == "road":
-            self.image = pygame.transform.scale(pygame.image.load('road.png'), (800, 600))
-        elif self.type == "water":
-            self.image = pygame.transform.scale(pygame.image.load('water.png'), (800, 600))
-        elif self.type == "grass":
-            self.image = pygame.transform.scale(pygame.image.load('grass.png'), (800, 600))
+        if height != None:
+            self.image = pygame.transform.scale(pygame.image.load('grass.png'), (800, height))
+        else:
+            if self.type == "road":
+                self.image = pygame.transform.scale(pygame.image.load('road.png'), (800, 600))
+            elif self.type == "water":
+                self.image = pygame.transform.scale(pygame.image.load('water.png'), (800, 600))
+            elif self.type == "grass":
+                self.image = pygame.transform.scale(pygame.image.load('grass.png'), (800, 600))
         self.rect = self.image.get_rect()
         self.rect.x = 0
         if y != None:
@@ -167,17 +213,21 @@ pygame.display.set_icon(icon)
 
 # creating the backgrounds
 backgrounds_group = pygame.sprite.Group()
-background = Background("grass", 600-100)
+background = Background("grass", 600-100, None)
 backgrounds_group.add(background)
+
+# creating a settings button
+settings_button = pygame.image.load('settings.png')
+resized_settings_button = pygame.transform.scale(settings_button, (50, 50))
 
 # creating the player
 player_image = pygame.image.load('frog.png')
-resized_player = pygame.transform.scale(player_image, (30, 30))
+resized_player = pygame.transform.scale(player_image, (35, 35))
 player = Player()
 
 # creating a test obstacle
-#obstacle_image = pygame.image.load('obstacle.png')
-#resized_obstacle = pygame.transform.scale(obstacle_image, (50, 50))
+obstacle_image = pygame.image.load('temporary_obstacle.png')
+resized_obstacle = pygame.transform.scale(obstacle_image, (100, 50))
 obstacle_group = pygame.sprite.Group()
 
 # creating a test log
@@ -189,6 +239,9 @@ log_group = pygame.sprite.Group()
 running = True
 change_in_y = 0
 clock = pygame.time.Clock()
+score = 0
+# score font
+font = pygame.font.Font(None, 36)
 while running:
 
     # background color
@@ -208,14 +261,13 @@ while running:
             elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 player.change_direction("right")
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_w or event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+            if event.key == pygame.K_w or event.key == pygame.K_UP or event.key == pygame.K_SPACE or event.key == pygame.K_s or event.key == pygame.K_DOWN or event.key == pygame.K_a or event.key == pygame.K_LEFT or event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 player.change_direction("")
-            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.change_direction("")
-            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.change_direction("")
-            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.change_direction("")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                # checking if the settings button was clicked
+                if screen.get_width()-resized_settings_button.get_width() < event.pos[0] < screen.get_width() and 0 < event.pos[1] < resized_settings_button.get_height():
+                    settings_screen()
     
     # checking if at least one background has y lower or equal to 0, if not, adding a new background
     temp = False
@@ -225,12 +277,18 @@ while running:
             temp = True
         else:
             back = background
-    #print(temp, back)
+
     if not temp and back != None:
-        background = Background(None, back.rect.y - screen.get_height())
-        backgrounds_group.add(background)
+        if back.type == "grass":
+            if back.image.get_height() == 200:
+                background1 = Background(None, back.rect.y - 600, None)
+            else:
+                background1 = Background(None, back.rect.y - back.image.get_height(), None)
+        else:
+            background1 = Background("grass", back.rect.y - 200, 200)
+        backgrounds_group.add(background1)
         # filling the background with obstacles or logs
-        fill_background(background)
+        fill_background(background1)
     
     # removing backgrounds that are out of the screen
     for background in backgrounds_group:
@@ -240,9 +298,9 @@ while running:
     # removing obstacles that are out of the screen
     for obstacle in obstacle_group:
         if obstacle.rect.x < -obstacle.rect.width or obstacle.rect.x > screen.get_width():
+            new_obstacle = Obstacle(obstacle.rect.y, obstacle.direction)
             obstacle_group.remove(obstacle)
-            obstacle = Obstacle()
-            obstacle_group.add(obstacle)
+            obstacle_group.add(new_obstacle)
 
     # removing logs that are out of the screen
     for log in log_group:
@@ -266,6 +324,9 @@ while running:
     for background in backgrounds_group:
         background.move()
     
+    # increasing the score
+    score += change_in_y
+    
     # resetting the change_in_y(if this is not done, the logs would never stop moving)
     change_in_y = 0
 
@@ -282,6 +343,13 @@ while running:
     # drawing the player
     player.draw(screen)
 
+    # drawing a settings button
+    screen.blit(resized_settings_button, (screen.get_width()-resized_settings_button.get_width(), 0))
+
+    # drawing the score in the top left corner
+    text = font.render("Score: "+str(score), True, (255, 255, 255))
+    screen.blit(text, (0, 0))
+
     # checking if player is on a log
     for log in log_group:
         if player.rect.x+player.rect.width < log.rect.x + log.rect.width and player.rect.x > log.rect.x:
@@ -291,17 +359,29 @@ while running:
                 log.has_player = False
         else:
             log.has_player = False
+    
+    # checking if player is on a water background and not on a log
+    if player.on_what == "water":
+        temp = False
+        for log in log_group:
+            if log.has_player:
+                temp = True
+                break
+        if not temp:
+            end_screen()
 
     # checking for collisions between player and obstacles
     if pygame.sprite.spritecollide(player, obstacle_group, False):
-        print("collision")
+        end_screen()
 
     # checking if player left the screen
     if player.rect.x < 0 - resized_player.get_width() or player.rect.x > screen.get_width():
-        print("player left the screen") # later this will trigger the end screen
+        end_screen()
 
+    # just for testing
     player.update()
 
+    # updating the screen and setting the frames per second
     clock.tick(60)
     pygame.display.update()
 
